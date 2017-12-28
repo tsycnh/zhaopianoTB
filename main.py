cde14710e5aaf3e87133880d4b35d1b33208592b
史丹利复合田 <tsycnh@126.com>
2017-12-28 23:21:11 +0800

    增加背景

--- main.py
+++ main.py
@@ -1,8 +1,9 @@
 from PIL import Image
 
 CANVAS_WIDTH = 1200
-IMAGE_WIDTH = int(1*CANVAS_WIDTH)
-
+IMAGE_RATIO = 0.9
+IMAGE_WIDTH = int(IMAGE_RATIO*CANVAS_WIDTH)
+BLOCK_GAP = int((1-IMAGE_RATIO)*CANVAS_WIDTH/2)
 IMAGES_LIST = [
 '淘宝/信.jpg',
 '淘宝/淘宝图1：放大镜1.jpg',
@@ -24,23 +25,36 @@ def load_images(file_list):
     return imgs
 class Canvas():
     def __init__(self,width):
-        self.bg = Image.open('background.png')
-        self.canvas = Image.new('RGB',size=(width,100),color=(255,0,0))#初始化大小
+        self.bg = self.formatImage(Image.open('background.png'),new_w=CANVAS_WIDTH)
+        # self.canvas = Image.new('RGB',size=(width,100),color=(255,0,0))#初始化大小
+        self.canvas = self.bg.crop(box=(0,0,width,100))
 
     def append(self,image):
         image = self.formatImage(image)
-        new_height = self.canvas.height + image.height
+        new_height = self.canvas.height + image.height + BLOCK_GAP
         new_canvas = Image.new('RGB',size=(self.canvas.width,new_height))
-        new_canvas.paste(self.canvas)
+        validbg = self.validBg(image.height+BLOCK_GAP)
+        new_bg = validbg.crop(box=(0, 0, CANVAS_WIDTH-1, image.height+BLOCK_GAP))
+        new_canvas.paste(self.canvas)#粘之前的图
         x = int((self.canvas.width-image.width)/2)
+        new_canvas.paste(new_bg,(0,self.canvas.height))
         new_canvas.paste(image,(x,self.canvas.height))
         self.canvas = new_canvas
 
-    def formatImage(self,img):
-        new_w = IMAGE_WIDTH
+    def formatImage(self,img,new_w = IMAGE_WIDTH):
+        # new_w = IMAGE_WIDTH
         new_h = int((new_w/img.width)*img.height)
         return img.resize(size=(new_w,new_h))
-
+    def validBg(self,asked_height):
+        i=2
+        bg = self.bg
+        while asked_height>bg.height:
+            new_bg = Image.new('RGB',size=(self.bg.width,self.bg.height*i))
+            new_bg.paste(bg,box=(0,0))
+            new_bg.paste(self.bg,box=(0,self.bg.height*(i-1)))
+            bg = new_bg
+            i+=1
+        return bg
     def export(self,save_to_disk = True):
         current_height = self.canvas.height
         if current_height%EXPORT_HEIGHT == 0:
@@ -59,16 +73,15 @@ class Canvas():
             ex_imgs.append(img)
         if save_to_disk:
             for key,item in enumerate(ex_imgs):
-                item.save('./淘宝图'+str(key)+'.jpg')
+                item.save('./输出/淘宝图'+str(key)+'.jpg')
         return ex_imgs
 
 imgs = load_images(IMAGES_LIST)
 
 c = Canvas(CANVAS_WIDTH)
-
+# bg = c.validBg(5000)
+# bg.show()
 for img in imgs:
     c.append(img)
-# c.canvas.show()
 ex_imgs = c.export()
-# for i in ex_imgs:
-#     i.show()
+
